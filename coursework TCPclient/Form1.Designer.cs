be0289036567cab89c7f76da17116f3e4ab39df6
Casey Tyler <keshakeshievich@gmail.com>
2025-01-23 19:10:13 +0700

    макет внешнего вида игры, описание кнопок NewGameButton и ContinueButton

--- coursework TCPclient/Form1.Designer.cs
+++ coursework TCPclient/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            NewGameButton = new Button();
+            ContinueButton = new Button();
+            textLines = new RichTextBox();
+            visual = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)visual).BeginInit();
+            SuspendLayout();
+            // 
+            // NewGameButton
+            // 
+            NewGameButton.BackColor = Color.LightBlue;
+            NewGameButton.Location = new Point(321, 131);
+            NewGameButton.Name = "NewGameButton";
+            NewGameButton.Size = new Size(157, 62);
+            NewGameButton.TabIndex = 0;
+            NewGameButton.Text = "Новая игра";
+            NewGameButton.UseVisualStyleBackColor = false;
+            NewGameButton.Click += NewGameButton_Click;
+            // 
+            // ContinueButton
+            // 
+            ContinueButton.BackColor = SystemColors.InactiveCaption;
+            ContinueButton.Location = new Point(321, 220);
+            ContinueButton.Name = "ContinueButton";
+            ContinueButton.Size = new Size(157, 58);
+            ContinueButton.TabIndex = 1;
+            ContinueButton.Text = "Продолжить";
+            ContinueButton.UseVisualStyleBackColor = false;
+            ContinueButton.Click += ContinueButton_Click;
+            // 
+            // textLines
+            // 
+            textLines.Location = new Point(22, 286);
+            textLines.Name = "textLines";
+            textLines.Size = new Size(752, 144);
+            textLines.TabIndex = 2;
+            textLines.Text = "";
+            textLines.Visible = false;
+            // 
+            // visual
+            // 
+            visual.Location = new Point(22, 12);
+            visual.Name = "visual";
+            visual.Size = new Size(752, 266);
+            visual.TabIndex = 3;
+            visual.TabStop = false;
+            visual.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(800, 450);
+            Controls.Add(visual);
+            Controls.Add(textLines);
+            Controls.Add(ContinueButton);
+            Controls.Add(NewGameButton);
+            Name = "Form1";
+            Text = "MainWindow";
+            ((System.ComponentModel.ISupportInitialize)visual).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button NewGameButton;
+        private Button ContinueButton;
+        private RichTextBox textLines;
+        private PictureBox visual;
     }
 }
--- coursework TCPclient/Form1.cs
+++ coursework TCPclient/Form1.cs
@@ -2,9 +2,37 @@ namespace coursework_TCPclient
 {
     public partial class Form1 : Form
     {
+        int AutosaveCode;
         public Form1()
         {
             InitializeComponent();
+            AutosaveCode = 0;
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            /*
+             вызывается метод TCPclient и устанавливается соединение с сервером
+            на сервер отправляется сообщение 'newgame' и сервер очищает список автосохранений, устанавливая текущий код
+            автосохранения 0, 
+            далее вызывается из бибилотеки GameLibrary статический метод класса GameLine
+            
+            после закрытия программы, на сервер отправляется сообщение 
+             */
+
+        }
+
+        private void ContinueButton_Click(object sender, EventArgs e)
+        {
+            /*
+             вызывается метод TCPclient и устанавливается соединение с сервером
+            на сервер отправляется сообщение 'contgame'
+            если список автосохранений пуст, то сервер возвращает сообщение 'noautosaves' и приложение высвечивает
+            messageBox с сообщением о том, что нет последних автосохранений
+            если список автосохранений не пуст, сервер возвращает код последнего автосохранения, который заносится в переменную
+            AutosaveCode
+            далее вызывается из бибилотеки GameLibrary статический метод класса GameLine
+             */
         }
     }
 }
